7ab8471e4092f1e3d7fa3318fc66a14cab31b289
ABHISHEK <56172425+imaxisXD@users.noreply.github.com>
2021-09-05 16:43:37 +0530

    Added : styling and useEffect

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# cash-app
-Created with CodeSandbox
+# Cash Registry
+
+##Problem Statement
+
+1. Your program has currencies of Rs. 1, 5, 10, 20, 100, 500, 2000.
+2. Your user enters a bill amount say Rs. 243.
+3. Your user then enters cash given say Rs. 2000.
+4. Now, help the user by telling how can he/she return the change to the customer with a minimum number of notes?
--- src/App.js
+++ src/App.js
@@ -1,55 +1,123 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./styles.css";
+
+const denominations = [2000, 500, 200, 100, 50, 20, 10, 5, 2, 1];
+let value = [];
 
 export default function App() {
   const [toggle, setToggle] = useState(false);
   const [userMsg, setUserMsg] = useState("");
-  const [bill, setBill] = useState(0);
-  //Bill amount handler function
-  function amountHandler(event) {
-    let billAmount = event.target.value;
+  const [bill, setBill] = useState();
+  const [cashGiven, setCashGiven] = useState();
+  const [btnFlag, setBtnFlag] = useState(false);
+  const [tableDisplay, setTableDisplay] = useState(false);
 
-    if (!isNaN(parseFloat(billAmount)) && isFinite(billAmount)) {
-      setToggle(true);
-      setBill(parseInt(billAmount, 10));
-      setUserMsg(" ");
-    } else {
-      setToggle(false);
-    }
+  function moneyClickHandler(event) {
+    setUserMsg("Wohoo ✨! Below is table for Minium Note ");
+    setTableDisplay(true);
+    calculateChange(cashGiven - bill);
   }
-  //Cash given handler function
-  function cashGivenHandler(event) {
-    let cashGiven = event.target.value;
-
-    if (!isNaN(parseFloat(cashGiven)) && isFinite(cashGiven)) {
+  function calculateChange(money) {
+    value = [];
+    denominations.forEach((note, i) => {
+      value.push(Math.trunc(money / note));
+      money %= note;
+    });
+  }
+  useEffect(() => {
+    if (cashGiven) {
       if (bill > cashGiven) {
-        setUserMsg("GOOD Plate dho do ab please");
-      } else if (cashGiven >= bill) {
-        setUserMsg("Click the button to generate the change");
+        setUserMsg(
+          "GOOD Plate dho do ab please 😂(cash given is less than bill)"
+        );
+        setTableDisplay(false);
+        setBtnFlag(false);
+      } else if (cashGiven === bill) {
+        setUserMsg("No Change as cash given is equal to bill amount 🤓");
+        setTableDisplay(false);
+        setBtnFlag(false);
+      } else {
+        setUserMsg("Click the button to generate the change 👇");
+        setBtnFlag(true);
       }
     } else {
-      setUserMsg("Enter a numeric value");
+      setUserMsg("Enter Cash Given 💰");
+      setBtnFlag(false);
+      setTableDisplay(false);
     }
-  }
+    if (!bill) {
+      setToggle(false);
+      setUserMsg("Enter Bill Amount 💰");
+      setCashGiven("");
+      setBtnFlag(false);
+      setTableDisplay(false);
+    }
+  }, [bill, cashGiven]);
+
+  useEffect(() => {
+    if (bill) {
+      setToggle(true);
+      setUserMsg("Enter Cash Given 💰");
+    } else {
+      setTableDisplay(false);
+      setBtnFlag(false);
+      setToggle(false);
+      setUserMsg("Enter Bill Amount 💰");
+      setCashGiven("");
+    }
+  }, [bill]);
 
   return (
     <div className="App">
-      <h1>Cash Registry</h1>
+      <h1>Cash Registry 💸</h1>
       <p>Let me help you in your billing</p>
       <div id="div-container">
         <input
-          onInput={amountHandler}
+          type="number"
+          step="any"
+          value={bill}
+          onChange={(event) => setBill(event.target.valueAsNumber)}
           placeholder="Enter the bill amount"
         ></input>
         <input
-          onInput={cashGivenHandler}
+          type="number"
+          step="any"
+          value={cashGiven}
+          onChange={(event) => setCashGiven(event.target.valueAsNumber)}
           style={{ display: toggle === false ? "none" : "block" }}
           placeholder="Enter the cash given"
         ></input>
+        <p>{userMsg}</p>
+        <button
+          id="btn-generate"
+          onClick={moneyClickHandler}
+          disabled={btnFlag === false ? true : ""}
+          style={{ display: toggle === false ? "none" : "block" }}
+        >
+          Generate
+        </button>
       </div>
-      <p>{userMsg}</p>
-      <button>Generate</button>
-      <div id="denomination-div"></div>
+      <div
+        id="denomination-div"
+        style={{ display: tableDisplay === false ? "none" : "block" }}
+      >
+        <table id="currency-table">
+          <thead>
+            <tr>
+              {denominations.map(function (note) {
+                return <th>{note}</th>;
+              })}
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              {value.map(function (val) {
+                return <td>{val}</td>;
+              })}
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
