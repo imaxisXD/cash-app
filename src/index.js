cea50593fa8a383706ca3ea043b699fdccaf8ae2
ABHISHEK <56172425+imaxisXD@users.noreply.github.com>
2021-09-05 17:11:35 +0530

    Added : footer

--- src/index.js
+++ src/index.js
@@ -7,6 +7,25 @@ const rootElement = document.getElementById("root");
 ReactDOM.render(
   <StrictMode>
     <App />
+    <footer class="footer">
+      <div class="footer-header">Connect with me</div>
+      <ul class="list-non-bullet">
+        <li class="list-item-inline secondary-link">
+          <a class="link" href="https://github.com/imaxisXD/">
+            <i class="fab fa-github fa-1x"></i> GitHub
+          </a>
+        </li>
+        <li class="list-item-inline primary-link">
+          <a class="link" href="https://www.linkedin.com/in/abhishek-ichi/">
+            <i class="fab fa-linkedin fa-1x"></i> LinkedIn
+          </a>
+        </li>
+      </ul>
+      <script
+        src="https://kit.fontawesome.com/a8bef643d9.js"
+        crossorigin="anonymous"
+      ></script>
+    </footer>
   </StrictMode>,
   rootElement
 );
